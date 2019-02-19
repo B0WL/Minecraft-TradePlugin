3ba53430012f1a0050d78076ba6be96ee54d424f
B0WL <kjiforouter@gmail.com>
2019-02-20 00:20:55 +0900

    경매장 gui 구성

--- Trade/src/main/java/command/CommandManager.java
+++ Trade/src/main/java/command/CommandManager.java
@@ -1,15 +1,23 @@
 package command;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
+import intenrnal.MenuInventory;
 import main.Trade;
 
 
 public class CommandManager implements CommandExecutor {
 	private Trade trade;
+	private MenuInventory menuInventory = new MenuInventory();
 	
 	public CommandManager(Trade trade) {
 		this.trade = trade;
@@ -18,15 +26,30 @@ public class CommandManager implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		
-	    if (cmd.getName().equalsIgnoreCase("KillPlayer")) {
-	        Player target = sender.getServer().getPlayer(args[0]);
-	         // Make sure the player is online.
-	        if (target == null) {
-	            sender.sendMessage(args[0] + " is not currently online.");
-	            return true;
-	        }
-	        target.setHealth(0.0D); 
+		if (args.length == 0) {
+			return true;
+		}
+		
+		if(args[0].equalsIgnoreCase("help")) {
+			sender.sendMessage(Trade.CHAT_PREFIX + " Commands:");
+			sender.sendMessage(ChatColor.WHITE + "/" + label + " Auction" + ChatColor.GRAY + " open Auction");
+	    	
+	    	return true;
+		}
+	    
+	    
+	    if(args[0].equalsIgnoreCase("Auction")) {
+	    	sender.sendMessage("Auction open");
+	    	menuInventory.onAuctionMain((Player)sender);
+	    	return true;
 	    }
+	    
+	    
+	    
+	    
+	    
+	    
+	    
 	    return false;
 	    
 	    

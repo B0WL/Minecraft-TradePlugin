52e28728382dd1d1a140f3fa9f3b658276cfd133
B0WL <kjiforouter@gmail.com>
2019-02-20 00:39:46 +0900

    Revert "Revert "경매장 gui 구성""
    This reverts commit 68d0756d43aefcfdf81f0e2f39acfea2be5e9aee.

--- Trade/src/main/java/main/Trade.java
+++ Trade/src/main/java/main/Trade.java
@@ -1,28 +1,43 @@
 package main;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import command.CommandManager;
+import listener.InventoryListener;
+import listener.ItemListener;
 
-public class Trade extends JavaPlugin{
+public class Trade extends JavaPlugin {
+
+	public static final String CHAT_PREFIX = ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "Trade" + ChatColor.DARK_GREEN + "] " + ChatColor.GREEN;
 
 	@Override
 	public void onEnable() {
 		// TODO Auto-generated method stub
+
+		try {
+			//getServer().getPluginManager().registerEvents(new ItemListener(), this);
+			getServer().getPluginManager().registerEvents(new InventoryListener(), this);
+		} catch (Exception e) {
+			getLogger().info("Item Listen Failed");
+		}
 		
-		this.getCommand("KillPlayer").setExecutor(new CommandManager(this));
 		
+		try {
+			this.getCommand("Trade").setExecutor(new CommandManager(this));
+		} catch (Exception e) {
+
+			getLogger().info("Command Listen Failed");
+		}
+
 		getLogger().info("Trade onEnable");
-		
+
 	}
 
-	
 	@Override
 	public void onDisable() {
 		// TODO Auto-generated method stub
 		getLogger().info("Trade onDisable");
 	}
-
-
 
 }
